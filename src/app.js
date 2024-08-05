409fa80eebe585fe360553dcfead29d7925f232f
vonzibon89 <vonzibon89@hotmail.com>
2024-08-05 17:33:08 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const sujeto = ["Mi perro", "Mi gato", "Mi hermano", "Mi primito"];
+const verbo = ["rompió", "se comió", "destrozó", "tiró"];
+const objeto = ["la tarea", "mi proyecto", "mis notas", "el cuaderno"];
+const tiempo = ["antes de ayer", "ayer", "hoy", "recién"];
+const emoji = ["😅", "😭", "🥲", "😔"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarExcusa() {
+  const sujetoAleatorio = sujeto[Math.floor(Math.random() * sujeto.length)];
+  const verboAleatorio = verbo[Math.floor(Math.random() * verbo.length)];
+  const objetoAleatorio = objeto[Math.floor(Math.random() * objeto.length)];
+  const tiempoAleatorio = tiempo[Math.floor(Math.random() * tiempo.length)];
+  const emojiAleatorio = emoji[Math.floor(Math.random() * emoji.length)];
+
+  return `${sujetoAleatorio} ${verboAleatorio} ${objetoAleatorio} ${tiempoAleatorio} ${emojiAleatorio}`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excusa = generarExcusa();
+  document.getElementById("resultado").innerText = excusa;
 };
